44f2307a79f2d2f27a346250cab8263439548146
redhuskie <jdpickering1@gmail.com>
2025-04-15 07:58:10 -0500

    Test Regression Model

--- main.py
+++ main.py
@@ -119,14 +119,18 @@ def go(config: DictConfig):
         )
 
         if "test_regression_model" in active_steps:
-             _ = mlflow.run(
-                f"{config['main']['components_repository']}/test_regression_model",
-                "main",
-                parameters={
-                    "mflow_model": "random_forest_export:prod",
-                    "test_dataset": "test_data.csv:latest"
-                }
-             )
+            
+            ##################
+            # Implement here #
+            ##################
+            mlflow.run(
+            os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+            "main",
+            parameters={
+                "mlflow_model": "random_forest_export:prod",  
+                "test_dataset": "test_data.csv:latest",       
+            },
+        )
             
             ##################
             # Implement here #
